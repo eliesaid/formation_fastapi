8e0685688440faccfb97ffcab2df1c55ada43d42
eliesaid <guerchom.said@gmail.com>
2025-09-18 14:19:39 +0200

    Ajout : Projet final 2

--- test_jour2/main3.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from fastapi import FastAPI
-from utils import router  # ✅ import du router défini dans utils.py
-
-app = FastAPI(title="App modulaire avec utils")
-
-# 🔗 Inclusion des routes externes
-app.include_router(router, prefix="/utils", tags=["utils"])
-
